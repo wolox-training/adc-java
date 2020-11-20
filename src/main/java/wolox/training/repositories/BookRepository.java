333820833bd6b204a125209192cf07bacb102cac
alexdavcudriz <alex.cudriz@wolox.co>
2020-11-20 14:52:05 -0500

    Add javadoc in book controller and repository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -6,6 +6,12 @@ import wolox.training.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    /**
+     * Method that obtain a book by its author
+     *
+     * @param author: Name of author (String)
+     * @return {@link Book}
+     */
     Optional<Book> findByAuthor(String author);
 
 }
