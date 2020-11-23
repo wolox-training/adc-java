dc786944a1692cfec79e0df015ecce7af5112d1d
alexdavcudriz <alex.cudriz@wolox.co>
2020-11-23 14:13:32 -0500

    create user model

---
+++ src/main/java/wolox/training/models/User.java
@@ -0,0 +1,77 @@
+package wolox.training.models;
+
+import com.sun.istack.NotNull;
+import wolox.training.exceptions.BookAlreadyOwnedException;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+@Table(name = "users")
+@Entity
+public class User {
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    @NotNull
+    @Column(nullable = false)
+    private String username;
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate birthdate;
+    @ManyToMany
+    private List<Book> books = Collections.emptyList();
+
+    public List<Book> getBooks() {
+        return Collections.unmodifiableList(books);
+    }
+
+    public void setBook(Book book) throws BookAlreadyOwnedException {
+        if (this.books.contains(book)) throw new BookAlreadyOwnedException();
+        this.books.add(book);
+    }
+    
+    public boolean removeBook(Book book) {
+        return books.remove(book);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
