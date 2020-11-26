b25b111cba65dbebe3090956bacc233526f7bb05
alexdavcudriz <alex.cudriz@wolox.co>
2020-11-26 09:20:48 -0500

    Created client models

---
+++ src/main/java/wolox/training/client/models/AuthorDto.java
@@ -0,0 +1,26 @@
+package wolox.training.client.models;
+
+public class AuthorDto {
+
+    private String url;
+    private String name;
+
+    public AuthorDto() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/wolox/training/client/models/PublisherDto.java
@@ -0,0 +1,17 @@
+package wolox.training.client.models;
+
+public class PublisherDto {
+
+    private String name;
+
+    public PublisherDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
